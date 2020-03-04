0aca748d5750ae287d2dc1bf5c9a0c7f371a19ee
lhr5201314 <61781070+lhr5201314@users.noreply.github.com>
2020-03-04 19:48:56 +0800

    Update and rename src/com/offcn/demo/Demo.java to SRC/COM/奥芬/演示/Demo.java

--- SRC/COM/奥芬/演示/Demo.java
+++ SRC/COM/奥芬/演示/Demo.java
@@ -3,8 +3,8 @@ package com.offcn.demo;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("xixi");
-        System.out.println("xixi");
+        System.out.println("haha");
+        System.out.println("haha");
         System.out.println("xixi");
         System.out.println("xixi");
     }
